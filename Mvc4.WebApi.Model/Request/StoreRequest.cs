a166cbe0a2ec5bdb061a91cfd771e7a2eb7f6555
Josh Cadruvi <jcadruvi78@yahoo.com>
2013-07-13 13:50:33 -0700

    Changed the servicemodel project to model.

--- Mvc4.WebApi.Model/Request/StoreRequest.cs
+++ Mvc4.WebApi.Model/Request/StoreRequest.cs
@@ -4,20 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Mvc4.WebApi.ServiceModel.Response
+namespace Mvc4.WebApi.ServiceModel.Request
 {
-    public class StoreResponse
+    public class StoreRequest
     {
         public int? RetailerId { get; set; }
-        public string RetailerName { get; set; }
         public int? Id { get; set; }
         public string Name { get; set; }
         public string Number { get; set; }
         public string City { get; set; }
         public string State { get; set; }
         public int? OrgLevelId { get; set; }
-        public string OrgLevelName { get; set; }
         public int? SubOrgLevelId { get; set; }
-        public string SubOrgLevelName { get; set; }
     }
 }
--- Mvc4.WebApi.Model/Response/StoreResponse.cs
+++ Mvc4.WebApi.Model/Response/StoreResponse.cs
@@ -4,17 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Mvc4.WebApi.ServiceModel.Request
+namespace Mvc4.WebApi.ServiceModel.Response
 {
-    public class StoreRequest
+    public class StoreResponse
     {
         public int? RetailerId { get; set; }
+        public string RetailerName { get; set; }
         public int? Id { get; set; }
         public string Name { get; set; }
         public string Number { get; set; }
         public string City { get; set; }
         public string State { get; set; }
         public int? OrgLevelId { get; set; }
+        public string OrgLevelName { get; set; }
         public int? SubOrgLevelId { get; set; }
+        public string SubOrgLevelName { get; set; }
     }
 }
